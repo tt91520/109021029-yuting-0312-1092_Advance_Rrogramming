7dfcc3cfb8940a51100f4dc0216c87366ec66627
tt91520 <109021029@live.asia.edu.tw>
2021-03-12 14:53:13 +0800

    0312

---
+++ demo.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, name, id):
+        self.studentName = name
+        self.studentId = id
+
+
+x = Student("簡車車", "109021089")
+print(x.studentName, "\t", x.studentId)
+
+
